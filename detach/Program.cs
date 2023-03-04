7addc6fe9bad4e57d7b660aa0c4ea8b2ab98f2db
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-03-04 17:11:55 +0100

    initial commit

---
+++ detach/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("No target was supplied. Exiting ...");
+    return;
+}
+
+string target = args[0];
+string arguments = string.Join(' ', args[1..].Select(a => $"\"{a}\""));
+ProcessStartInfo pinfo = new(target, arguments)
+{
+    UseShellExecute = true
+};
+Process.Start(pinfo);
